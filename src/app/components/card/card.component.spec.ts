a6ae138e6f8be43c8d42fd4c73f600ff340de906
Renan Vicente <renanvicentecosta@gmail.com>
2024-07-25 00:09:33 -0300

    chore: add tests to card

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -8,14 +8,29 @@ describe('CardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CardComponent]
+      imports: [CardComponent]
     });
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create and set properties', () => {
+    component.title = 'Test title';
+    component.position = 1;
+    component.uploadSpeed = 10;
+    component.downloadSpeed = 20;
+    component.price = 20;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+
     expect(component).toBeTruthy();
+    expect(compiled.querySelector('.card-title').textContent).toContain('Test title');
+    expect(compiled.querySelector('.card-index').textContent).toContain('1');
+    expect(compiled.querySelector('.card-speed-upload').textContent).toContain('10 Mbit/s');
+    expect(compiled.querySelector('.card-speed-download').textContent).toContain('20 Mbit/s');
+    expect(compiled.querySelector('.card-price').textContent).toContain('20,00 €');
   });
+
+
 });
