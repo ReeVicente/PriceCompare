bf149e5be3c97404b4238489a33ea994b4c9ea74
Renan Vicente <renanvicentecosta@gmail.com>
2024-07-25 02:25:08 -0300

    feat: design home screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {CardComponent} from "./components/card/card.component";
+import { FilterButtonComponent } from './components/filter-button/filter-button.component';
+import { SelectComponent } from './components/select/select.component';
+import { FilterCardComponent } from './components/filter-card/filter-card.component';
+import { InputComponent } from './components/input/input.component';
+import {ModalComponent} from "./components/modal/modal.component";
+import {ButtonComponent} from "./components/button/button.component";
 
 @NgModule({
   declarations: [
@@ -11,6 +17,12 @@ import {CardComponent} from "./components/card/card.component";
   imports: [
     BrowserModule,
     CardComponent,
+    FilterButtonComponent,
+    SelectComponent,
+    FilterCardComponent,
+    InputComponent,
+    ModalComponent,
+    ButtonComponent,
   ],
   providers: [],
   exports: [],
