38984924b8be9f3a1b8362f61705a979229ff457
Renan Vicente <renanvicentecosta@gmail.com>
2024-07-25 02:32:02 -0300

    chore: add button handler

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,4 +7,9 @@ import { Component, Input } from '@angular/core';
   standalone: true
 })
 export class ButtonComponent {
+  @Output() onClick = new EventEmitter();
+
+  protected onClickHandler() {
+    this.onClick.emit();
+  }
 }
